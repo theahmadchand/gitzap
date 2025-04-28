78433ff886b528a607e18dd683ad43dad78cdfea
theahmadchand <theahmadchand@gmail.com>
2025-04-28 11:34:45 +0500

    feat: add version command to gitzap for displaying the current version

--- scripts/gitzap.js
+++ scripts/gitzap.js
@@ -8,6 +8,13 @@ const command = process.argv[2];
 const scriptPath = path.join(os.homedir(), ".gitzap", "create-branch.sh");
 const pruneScriptPath = path.join(os.homedir(), ".gitzap", "prune-branches.sh");
 
+let version = "unknown";
+if (fs.existsSync(packageJsonPath)) {
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  version = packageJson.version;
+}
+
+
 switch (command) {
   case "create-branch":
     execSync(`bash ${scriptPath}`, { stdio: "inherit", shell: "/bin/bash" });
@@ -15,7 +22,14 @@ switch (command) {
   case "prune-branches":
     execSync(`bash ${pruneScriptPath}`, { stdio: "inherit", shell: "/bin/bash" });
     break;
+  case "--version":
+  case "-v":
+    console.log(`Gitzap version: ${version}`);
+    break;
   default:
-    console.log("Usage: gitzap create-branch | gitzap prune-branches");
+    console.log("Usage:");
+    console.log("  gitzap create-branch       Create a new Git branch");
+    console.log("  gitzap prune-branches      Prune merged Git branches");
+    console.log("  gitzap --version, -v       Show Gitzap version");
     process.exit(1);
 }
